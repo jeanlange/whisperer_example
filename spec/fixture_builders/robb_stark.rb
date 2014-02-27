4c5ed8310412ead2b8e241da9392f9d9caee6cb6
Dmitriy Nesteryuk <dmitriy.nesteryuk@ecommerce.com>
2014-02-27 19:36:36 +0200

    create a simple example for using whisperer gem

---
+++ spec/fixture_builders/robb_stark.rb
@@ -0,0 +1,28 @@
+Whisperer.define(:robb_stark) do
+  request do
+    uri    'http://example.com/users/1'
+    method :get
+
+    headers do
+      accept '*/*'
+    end
+  end
+
+  response do
+    status do
+      code    200
+      message 'OK'
+    end
+
+    headers do
+      content_type           'application/json;charset=utf-8'
+      content_length         57
+      x_content_type_options 'nosniff'
+    end
+
+    body do
+      encoding 'UTF-8'
+      factory  'robb_stark', :json
+    end
+  end
+end
